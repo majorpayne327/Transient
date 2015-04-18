56b2eaef2e06025a3a1526129ca64760c842da96
Chris Enoch <majorpayne327@gmail.com>
2015-04-18 17:03:10 -0400

    Continued with UI-Routing
    -Need to change css to apply correct styles.

--- app/js/app.js
+++ app/js/app.js
@@ -1,8 +1,23 @@
-var app = angular.module('transientApp', ['ngRoute']);
+var app = angular.module('transientApp', ['ui.router']);
 
-app.config(['$uiProvider', function ($routeProvider) {
-	$routeProvider.when('/', {
+app.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+
+	$urlRouterProvider.otherwise('/');
+	
+	$stateProvider.state('search', {
+		url: '/',
 		templateUrl: '/js/search/search.html'
-	}
-
-)}]);
+	}).state('result', {
+		templateUrl: '/js/appbase/appBase.html'
+	}).state('result.results', {
+		url: '/{location}',
+		templateUrl: '/js/home/HomePage.html'
+	}).state('result.category', {
+		url: '/{location}/{category}',
+		templateUrl: '/js/entertainment/Entertainment.html'
+	}).state('result.store', {
+		url: '/{location}/{category}/{store}',
+		templateUrl: '/js/diner/jaysDiner.html'
+	});
+	
+	}]);
